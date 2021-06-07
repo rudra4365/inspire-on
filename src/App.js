c2d261d41206c36f4b413ae767043c3ce9aa5ead
Rudra <rudra4365@gmail.com>
2021-06-07 19:41:53 +0530

    added radio button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Layout, Button, List } from 'antd'
 import { DownloadOutlined } from '@ant-design/icons'
 import './App.css';
 import Quote from './Quote'
 import Swiper from './Swiper'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 const config = {
   apiUrl: 'https://type.fit/api/quotes'
@@ -15,6 +20,19 @@ function App() {
 
   const [quotes, setQuotes] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+
+
+  // To call the API to receive all the quotes 
+  useEffect(() => {
+    getQuotes();
+    return;
+  }, [])
+
+  const [value, setValue] = React.useState('female');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   
   const getQuotes = () => {
     setQuotes([])
@@ -36,6 +54,15 @@ function App() {
     <div className="App">
       <Swiper qts = {quotes} />
       <div className = "main-heading">Inspire-ON</div>
+      <FormControl component="fieldset">
+      <FormLabel component="legend">Gender</FormLabel>
+      <RadioGroup row aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        {/* <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" /> */}
+      </RadioGroup>
+      </FormControl>
       <Layout>
       <div className="container">
         <List
